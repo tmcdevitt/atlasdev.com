018c21b85bd945b68114fe8d9bf9271eff12581f
Tom McDevitt <TMcDevitt@atlasdev.com>
2014-12-16 17:21:03 -0500

    Build

---
+++ TrackviaAPI/Form1.Designer.vb
@@ -0,0 +1,50 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnPost = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnPost
+        '
+        Me.btnPost.Location = New System.Drawing.Point(477, 35)
+        Me.btnPost.Name = "btnPost"
+        Me.btnPost.Size = New System.Drawing.Size(148, 23)
+        Me.btnPost.TabIndex = 0
+        Me.btnPost.Text = "Post"
+        Me.btnPost.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(704, 263)
+        Me.Controls.Add(Me.btnPost)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents btnPost As System.Windows.Forms.Button
+
+End Class
---
+++ TrackviaAPI/Form1.vb
@@ -0,0 +1,74 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Net
+Imports System.IO
+Imports System.Text
+
+
+Public Class Form1
+
+
+    ' Setup the variables necessary to make the Request
+    Dim grantType As String = "password"
+    Dim applicationid As String = "{applicationId}"
+    Dim clientid As String = "311_4y6ok3vk6bokswwc4ow0gkgsc4wwcs8goksskgokskgkoocwc0"
+    Dim clientsecret As String = "2rgmb4110qas0osogcwwowgck4wso0g8g0kokksgo4cwwksk4o"
+    Dim username As String = "tmcdevitt@atlasdev.com"
+    Dim password As String = "sunshine"
+    Dim url As String = "https://api.trackvia.com/oauth/v2/token?"
+    Dim response As HttpWebResponse = Nothing
+
+
+    Private Sub btnPost_Click(sender As Object, e As EventArgs) Handles btnPost.Click
+
+        'https://api.trackvia.com/oauth/v2/token?
+        'client_id={CLIENT_ID}&
+        'client_secret={CLIENT_SECRET}&
+        'grant_type = password&
+        'username={USERNAME}&
+        'password = {password}
+
+
+        
+
+        ' Create the data to send
+        Dim data As StringBuilder = New StringBuilder()
+        data.Append("client_id=" + Uri.EscapeDataString(clientid))
+        data.Append("&client_secret=" + Uri.EscapeDataString(clientsecret))
+        data.Append("&grant_type=" + Uri.EscapeDataString(grantType))
+        data.Append("&username=" + Uri.EscapeDataString(username))
+        data.Append("&password=" + Uri.EscapeDataString(password))
+
+        ' Create a byte array of the data to be sent
+        Dim byteArray As Byte() = UTF8Encoding.UTF8.GetBytes(data.ToString())
+
+        ' Setup the Request
+        Dim request As HttpWebRequest = WebRequest.Create(url)
+        request.Method = "POST"
+        request.ContentType = "application/x-www-form-urlencoded"
+        request.ContentLength = byteArray.Length
+
+        ' Write data
+        Dim postStream As Stream = request.GetRequestStream()
+        postStream.Write(byteArray, 0, byteArray.Length)
+        postStream.Close()
+
+        ' Send Request & Get Response
+        response = request.GetResponse()
+
+        ' Get the Response Stream
+        Dim reader As StreamReader = New StreamReader(response.GetResponseStream())
+        Dim json As String = reader.ReadLine()
+        Console.WriteLine(json)
+
+        ' Retrieve the Access Token
+        'Dim ser As JavaScriptSerializer = New JavaScriptSerializer()
+        'Dim x As Dictionary(Of String, Object) = ser.DeserializeObject(json)
+        'Dim accessToken As String = x("access_token").ToString()
+
+    End Sub
+
+
+
+
+End Class
